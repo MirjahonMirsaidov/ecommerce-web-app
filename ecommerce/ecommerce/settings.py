61f0bbf0c38a0ded13482eb3ac52bc7f264e5bfd
MirjahonMirsaidov <mirjahonac021@gmail.com>
2021-06-02 10:06:49 +0500

    h

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -148,7 +148,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = 'http://176.99.11.80:8040/media/'
+# MEDIA_URL = 'http://176.99.11.80:8040/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'main.User'
