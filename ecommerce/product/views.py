9b04b2bca9a26a7d3e90eaa8e1a4103d7318db60
MirjahonMirsaidov <mirjahonac021@gmail.com>
2021-06-02 09:18:47 +0500

    h

--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -582,7 +582,7 @@ class ProductDetailView(generics.GenericAPIView):
                     "categories": get_categories(parent),
                     "attributes": get_attributes(parent.id),
                     "price": parent.price,
-                    "image": "http://bahja-api.atomlab.uz" + parent.image.url,
+                    "image": "http://176.99.11.80:8040" + parent.image.url,
                     "quantity": parent.quantity,
                     "images": get_images(parent),
                     "created_at": parent.created_at,
@@ -605,7 +605,7 @@ class ProductDetailView(generics.GenericAPIView):
                 "categories": get_categories(variation),
                 "attributes": get_attributes(variation.id),
                 "price": variation.price,
-                "image": "http://bahja-api.atomlab.uz" + variation.image.url,
+                "image": "http://176.99.11.80:8040" + variation.image.url,
                 "quantity": variation.quantity,
                 "images": get_images(variation),
                 "created_at": variation.created_at,
@@ -639,7 +639,7 @@ class ProductSpecificDetailView(generics.GenericAPIView):
                 "description": product.description,
                 "price": product.price,
                 "quantity": product.quantity,
-                "image": "http://bahja-api.atomlab.uz" + product.image.url,
+                "image": "http://176.99.11.80:8040" + product.image.url,
                 "images": get_images(product),
             })
         except:
