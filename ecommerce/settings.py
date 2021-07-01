978a5d5549c10aa1ba13f1e4141c84af69825b6b
MirjahonMirsaidov <mirjahonac021@gmail.com>
2021-07-01 11:30:00 +0500

    setup

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'u#0q04ozathu)4ltl&&-lh9%dr+pv*e)a$0(@0gv(cs_#w2j2s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['dashboard.heroku.com', '127.0.0.1']
 CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
